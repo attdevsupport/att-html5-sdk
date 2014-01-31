a8ae8f119d7aee2d83fd711ee5c3c0afbf7e8bf1
Bruce Williams <brucew@isoftstone.com>
2014-01-31 18:47:58 -0500

    replace obsolete and non-existent send_data method with equivalent current code

--- working/server/ruby/example/app.rb
+++ working/server/ruby/example/app.rb
@@ -464,7 +464,8 @@ post '/speech/v3/textToSpeech' do
   text = URI.decode request['text']
   tts = Service::TTSService.new($config['apiHost'], $client_token)
   response = tts.toSpeech(text)
-  send_data response.data, :type => response.type
+  content_type response.type
+  response.data
 end
 
 post '/att/tl/getdevicelocation' do
