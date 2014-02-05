c7a3b52d5c965e2dfcced52a2a1c3d144d533f27
Bruce Williams <brucew@isoftstone.com>
2014-02-05 16:07:40 -0500

    enable some commented-out tests

--- test/samples/src/main/java/com/att/html5sdk/TestSpeech.java
+++ test/samples/src/main/java/com/att/html5sdk/TestSpeech.java
@@ -6,8 +6,8 @@ public class TestSpeech {
 	
 	public static void Execute() throws InterruptedException, IOException 
   {
-		// SpeechApp1positive SpeechApp1 = new SpeechApp1positive();
-		// SpeechApp1.Execute("ext-button-1", "ext-button-2");
+		SpeechApp1positive SpeechApp1 = new SpeechApp1positive();
+		SpeechApp1.Execute("ext-button-1", "ext-button-2");
     
     TestSpeechRecursive speech = new TestSpeechRecursive();
     speech.Execute();
