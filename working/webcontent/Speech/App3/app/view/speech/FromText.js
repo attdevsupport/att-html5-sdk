044653001422882579439bb99356ccf98df77a34
Bruce Williams <brucew@isoftstone.com>
2014-02-06 11:45:55 -0500

    restore inadvertantly-deleted files

---
+++ working/webcontent/Speech/App3/app/view/speech/FromText.js
@@ -0,0 +1,82 @@
+/**
+ *
+ * User Interface for the Text to Speech application.
+ *
+ */
+Ext.define('SampleApp.view.speech.FromText', {
+	extend: 'Ext.Container',
+	xtype: 'att-speech-fromtext',
+
+	requires: [
+        'Ext.form.Panel',
+        'Ext.form.FieldSet',
+        'SampleApp.view.Header',
+        'SampleApp.view.Footer'
+	],
+
+	config: {
+		title: 'Speech from Text',
+		scrollable: 'vertical',
+		defaults: { scrollable: null }
+	},
+
+	initialize: function () {
+		this.add([
+            { xtype: 'att-header' },
+            this.buildForm(),
+            { xtype: 'att-footer' }
+		]);
+	},
+
+	/**
+     * Builds the UI components for Feature 1: Send Speech To Text.
+     */
+	buildForm: function () {
+		return {
+
+			xtype: 'formpanel',
+			defaults: { margin: 30 },
+			items: [
+				{
+					xtype: 'fieldset',
+					title: 'Type text below to convert to speech',
+					layout: 'vbox',
+					maxWidth: 700,
+					items: [
+						{
+							xtype: 'textareafield',
+							name: 'textToConvert',
+							padding: 10,
+						}, {
+							xtype: 'container',
+							layout: 'hbox',
+							defaults: {margin: 20},
+							items: [
+								{
+									width: 120,
+									xtype: 'button',
+									action: 'submitText',
+									text: 'Submit',
+									disabled: true
+								}, {
+									xtype: 'button',
+									action: 'playConvertedSpeech',
+									text: "Play converted speech",
+									disabled: true
+								}
+							]
+						}, {
+							xtype: 'fieldset',
+							title: 'Result',
+							margin: 20,
+							height: 200,
+							html: '<div id="resultWindow" class="logWindow"></div>'
+						}
+					]
+				}
+			]
+		}
+	}
+});
+
+
