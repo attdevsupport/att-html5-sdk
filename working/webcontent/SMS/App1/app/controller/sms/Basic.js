1558de8b039f7dae621d70f77a8aa088d5e410b4
Nishita <nishita@pioneer-inc.com>
2014-02-19 22:18:43 -0800

    change receiveSms with getSms

--- working/webcontent/SMS/App1/app/controller/sms/Basic.js
+++ working/webcontent/SMS/App1/app/controller/sms/Basic.js
@@ -178,7 +178,7 @@ Ext.define('SampleApp.controller.sms.Basic', {
         
         view.setMasked(true);
         
-		AttApiClient["receiveSms"](
+		AttApiClient["getSms"](
 			{ shortcode: registrationId },
 			function (response) {
                 view.setMasked(false);
