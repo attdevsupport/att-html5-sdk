0085305023c44fbc111cbe5cd8c51c4308a228a4
Moiz Nizamuddin <moizn@isoftstone.com>
2014-02-20 21:48:34 -0800

    Update of Selenium and qUnit tests to include SMS tests for SendSMS and GetSMSStatus

--- working/webcontent/client/tests/SMS/Basic.js
+++ working/webcontent/client/tests/SMS/Basic.js
@@ -13,40 +13,43 @@
 	function slowFn(code) {
 		setTimeout(code, 200);
 	}
-		
+		var jsonObj = {
+					ClientApp: 'TestApp1'
+						}
 	function basicSMSTests(cfg) {
 		//Tests sending SMS
         slowTest("sendSms", function() {
-			provider.sendSms({
-				address: cfg.phoneNumberPrimary,
+			AttApiClient.sendSms({
+				addresses: cfg.primaryTestPhoneNumber,
 				message: "test sending SMS from client side",
-				success: function(response){
+				xarg     : jsonObj},
+				function(response){
 					start();
 					validateSmsResponse(response);
 				},
-				failure: function(response){
+				function(response){
 					start();
 					ok(false, "Fail on sending SMS message." +
 						"\nresponse: " + JSON.stringify(response));	
 				}
-			});
+			);
 			stop();
         });
 		
         slowTest("sendSms Football to personal cell phone", function() {
-			provider.sendSms({
-				address: cfg.personalCellPhone,
-				message: "Football",
-				success: function(response){
+			AttApiClient.sendSms({
+				addresses: cfg.primaryTestPhoneNumber,
+				message: "Football"},
+				function(response){
 					start();
-					validateSmsResponseWithMessage(response, "personal phone: "+ cfg.personalCellPhone);
+					validateSmsResponseWithMessage(response, "personal phone: "+ cfg.primaryTestPhoneNumber);
 				},
-				failure: function(response){
+				function(response){
 					start();
 					ok(false, "Fail on sending SMS message Football to personal cell phone." +
 											"\nresponse: " + JSON.stringify(response));	
 				}
-			});
+			);
 			stop();
         });
 		
@@ -56,93 +59,93 @@
 		 */
         slowTest("smsStatus", function() {
         	var messageTest = "SmsTest" + Math.random().toString();
-        	provider.sendSms({
-				address : cfg.phoneNumberPrimary, 
-				message : messageTest, 
-        		success  : function(response) {
+        	AttApiClient.sendSms({
+				addresses : cfg.primaryTestPhoneNumber, 
+				message : cfg.smsStatusMessage}, 
+        		function(response) {
         			start();
         			slowFn(function() {
-        				provider.getSmsStatus({
-							smsId   : response.Id, 
-	        				success : function(response) {
+        				AttApiClient.smsStatus({
+							id   : response["outboundSMSResponse"]["messageId"]},
+	        				function(response) {
 	        					start();
 								validateStatusResponse(response);
 	        				},
-	        				failure : function(response) {
+	        				function(response) {
 	        					start();
 	        					ok(false, "Fail on checking status of an SMS message." +
 											"\nresponse: " + JSON.stringify(response));	
 	        				}
-        				});
+        				);
     				}); 
 					stop();
         		},
-        		failure  : function(response) {
+        		function(response) {
         			start();
                     ok(false, "Fail on sending SMS message for checking status." +
 											"\nresponse: " + JSON.stringify(response));	
         		}
-        	});
+        	);
         	stop();
         });
 		
 		//Tests getting messages for the shortcode of the application		
 		slowTest("smsGetMessages", function() {
-			// alert("Working with smsGetMessages. \nThis might work with a valid RegistrationID.");
-			provider.receiveSms({
-				RegistrationID: 491,
-				success : function(response) {
+			// alert("Working with smsGetMessages. \nThis might work with a valid shortcode.");
+			AttApiClient.getSms({
+				shortcode: '44629356'},
+				function(response) {
 					start();
 					ok(false, "Strangely succeeded on getting messages sent to shortcode of application." +
 						"\nresponse: " + JSON.stringify(response));	
 					validateGetSmsResponse(response);
 				},
-				failure : function(response) {
+				function(response) {
 					start();
 					ok(false, "Fail on getting messages sent to shortcode of application." +
 						"\nresponse: " + JSON.stringify(response));	
 				}
-			});
+			);
 			stop();
 		});
 		
-		//Tests getting messages for the shortcode of the application; uses old, invalid shortcode.
+/* 		//Tests getting messages for the shortcode of the application; uses old, invalid shortcode.
 		slowTest("smsGetMessages from inactive shortcode", function() {
 			// alert("Working with smsGetMessages. \nThis might work with a valid RegistrationID.");
-			provider.receiveSms({
-				RegistrationID: cfg.inactiveShortcode, 
-				success : function(response) {
+			AttApiClient.getSms({
+				RegistrationID: cfg.inactiveShortcode}, 
+				function(response) {
 					start();
 					ok(false, "Succeeded in getting messages sent to an old, invalid shortcode." +
 						"\nresponse: " + JSON.stringify(response));	
 					validateGetSmsResponse(response);
 				},
-				failure : function(response) {
+				function(response) {
 					start();
 					ok(false, "Failed to get messages sent to shortcode of application." +
 						"\nresponse: " + JSON.stringify(response));	
 				}
-			});
+			);
 			stop();
-		});
+		}); */
 		
-		//Tests getting messages for the shortcode of the application		
+/* 		//Tests getting messages for the shortcode of the application		
 		slowTest("smsGetMessages from active shortcode", function() {
 			// alert("Working with smsGetMessages. \nThis might work with a valid RegistrationID.");
-			provider.receiveSms({
-				RegistrationID: cfg.activeShortcode, 
-				success : function(response) {
+			AttApiClient.getSms({
+				RegistrationID: cfg.activeShortcode}, 
+				function(response) {
 					start();
 					ok(true, "Succeeded in getting messages sent to the application shortcode." +
 						"\nresponse: " + JSON.stringify(response));	
 					validateGetSmsResponse(response);
 				},
-				failure : function(response) {
+				function(response) {
 					start();
 					ok(false, "Failed to get messages sent to shortcode of application." +
 						"\nresponse: " + JSON.stringify(response));	
 				}
-			});
+			);
 			stop();
-		});
+		}); */
 	}
