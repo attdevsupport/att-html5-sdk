411dfe15483889de8e7a488f319d00f4bf288ca3
Bruce Williams <brucew@isoftstone.com>
2014-02-22 21:52:39 -0500

    use 'working' to package servers

--- sdk/package.sh
+++ sdk/package.sh
@@ -13,17 +13,21 @@ cd sample/build/
 sh buildSamples.sh
 cd ../..
 
+# use the 'working' directory to package the server docs
+
 echo "building Ruby"
-cd server/ruby/
+cd ../working/server/ruby/
 sh buildDocs.sh
-cd ../..
 
 echo "building PHP"
-cd server/php/
+cd ../php/
 sh buildDocs.sh
 cd ../..
 
 echo "building Java"
 ant -buildfile server/java/build.xml clean package
 
+# switch back from 'working' to 'sdk' to complete the packaging
+
+cd ../sdk
 ant clean package
