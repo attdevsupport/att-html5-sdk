ac3520a66b62c3275191b0e3e736d1e5bbc0a6e9
Bruce Williams <brucew@isoftstone.com>
2014-02-27 10:58:31 -0500

    second attempt at separate doc packaging

--- working/server/ruby/buildDocs.sh
+++ working/server/ruby/buildDocs.sh
@@ -3,5 +3,5 @@ echo "---------------------------------"
 echo "- building Ruby docs for SDK ----"
 echo "---------------------------------"
 echo ""
-rdoc --output=../../../packaged.docs/server/ruby example
+rdoc --output ../../../packaged.docs/server/ruby example
 cp -R doc_src/resources/* ../../../packaged.docs/server/ruby/resources/
