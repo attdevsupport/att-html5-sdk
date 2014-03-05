234ce7287af7a68704346c8f25183241cdbaad44
Nishita <nishita@pioneer-inc.com>
2014-03-05 08:10:52 -0800

    add support to check multiple scopes in the session tokens

--- working/server/php/public_html/att/check.php
+++ working/server/php/public_html/att/check.php
@@ -1,15 +1,26 @@
 <?php
 
-require_once("../config.php");
+require_once("config.php");
 
 $bool 		 = "false";
-$scope 		 = isset($_GET['scope']) ? $_GET['scope'] : '';
-$tokens		 = isset($_SESSION['tokens']) ? $_SESSION['tokens'] : '';
+if (isset($_GET['scope'])) {
+	$tokens		 = isset($_SESSION['tokens']) ? $_SESSION['tokens'] : '';
 
-if (is_array($tokens)) {
-	foreach ($tokens as $key => $value) {
-		if ($key == $scope) {
-			$bool = "true";
+	$scopes = explode(",", $_GET['scope']);
+
+	if (is_array($tokens)) {
+		foreach($scopes as $s) {
+			$temp_bool = false;
+			foreach ($tokens as $key => $value) {
+				if ($key == $s) {
+					$bool = "true";
+					$temp_bool = true;
+				}
+			}
+			if (!$temp_bool) {
+				$bool = "false";
+				break;
+			}
 		}
 	}
 }
