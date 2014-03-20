fa15df76709d5eb065fb7be921879b0d1f0c03d0
Bruce Williams <brucew@isoftstone.com>
2014-03-20 13:11:32 -0400

    reduce duplicate code

--- working/server/ruby/att/services/ads.rb
+++ working/server/ruby/att/services/ads.rb
@@ -13,20 +13,10 @@ get '/att/rest/1/ads' do
   # construct optional parameters from query string
   #
   opts = {}
-  opts[:AgeGroup] = URI.decode(request.GET['AgeGroup']) if request.GET['AgeGroup']
-  opts[:AreaCode] = URI.decode(request.GET['AreaCode']) if request.GET['AreaCode']
-  opts[:City] = URI.decode(request.GET['City']) if request.GET['City']
-  opts[:Country] = URI.decode(request.GET['Country']) if request.GET['Country']
-  opts[:Gender] = URI.decode(request.GET['Gender']) if request.GET['Gender']
-  opts[:Keywords] = URI.decode(request.GET['Keywords']) if request.GET['Keywords']
-  opts[:Latitude] = URI.decode(request.GET['Latitude']) if request.GET['Latitude']
-  opts[:Longitude] = URI.decode(request.GET['Longitude']) if request.GET['Longitude']
-  opts[:MaxHeight] = URI.decode(request.GET['MaxHeight']) if request.GET['MaxHeight']
-  opts[:MaxWidth] = URI.decode(request.GET['MaxWidth']) if request.GET['MaxWidth']
-  opts[:MinHeight] = URI.decode(request.GET['MinHeight']) if request.GET['MinHeight']
-  opts[:MinWidth] = URI.decode(request.GET['MinWidth']) if request.GET['MinWidth']
-  opts[:Type] = URI.decode(request.GET['Type']) if request.GET['Type']
-  opts[:ZipCode] = URI.decode(request.GET['ZipCode']) if request.GET['ZipCode']
+  [:AgeGroup, :AreaCode, :City, :Country, :Gender, :Keywords, :Latitude, :Longitude, :MaxHeight, :MaxWidth, :MinHeight, :MinWidth, :Type, :ZipCode].each do |valid_param|
+    actual_param = request.GET[valid_param.to_s]
+    opts[valid_param] = URI.decode(actual_param) unless actual_param.nil?
+  end
   
   svc = Service::ADSService.new($config['apiHost'], $client_token, :raw_response => true)
   svc.getAds(category, user_agent, udid, opts)
