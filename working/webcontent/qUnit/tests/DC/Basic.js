50b09748dbe16629f4083df7c88159c50c72d9e1
Moiz Nizamuddin <moizn@isoftstone.com>
2014-03-26 15:51:42 -0700

    Fixed DC test

--- working/webcontent/qUnit/tests/DC/Basic.js
+++ working/webcontent/qUnit/tests/DC/Basic.js
@@ -1,4 +1,20 @@
-	function basicDCTests(cfg) {	
+	//Function that wraps all of the tests. Slows the tests for throttling purposes.
+	function slowTest(name, code) {
+		test(name, function() {
+			slowFn(function() {
+				start();
+				code();
+			});
+			stop();
+		})
+	}
+	
+	//Function inside the slowTest function that allows manipulation of the throttling time.
+	function slowFn(code) {
+		setTimeout(code, 200);
+	}
+    
+	function basicDCTests() {	
 		//Tests getting DC	
 		slowTest("deviceCapabilities", function() {
 			AttApiClient.getDeviceInfo(
@@ -8,7 +24,7 @@
 				},
 				function(response) {
 					start();
-					ok(false, "Fail on getting single DC");
+					ok(false, "Fail on getting single DC: " + JSON.stringify(response));
 				}
 			);
 			stop();
