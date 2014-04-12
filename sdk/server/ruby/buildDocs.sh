2033d3171efe092c7672b812cc08a4c1aae70fc3
Bruce Williams <brucew@isoftstone.com>
2014-04-12 17:25:35 -0400

    update doc tool

--- sdk/server/ruby/buildDocs.sh
+++ sdk/server/ruby/buildDocs.sh
@@ -1,18 +1,14 @@
 echo ""
 echo "----------------------------------------"
-echo "- building Ruby JSDuck docs for SDK ----"
+echo "- building Ruby server docs for SDK ----"
 echo "----------------------------------------"
 echo ""
 mkdir --parent ../../../packaged.docs/server/ruby
-jsduck \
-  ./att/*.rb \
-  ./att/services/*.rb \
-  -o ../../../packaged.docs/server/ruby \
+yardoc \
   --verbose \
-  --title="Ruby Documentation for the AT&T API Platform SDK for HTML5" \
-  --warnings=-all \
-  --welcome=./doc_src/welcome.html \
-  --head-html='<style type="text/css">#header-content{ background-image: url(../assets/icon.png); padding-left: 27px; }</style>'
-  
-cp -R doc_src/resources/* ../../../packaged.docs/server/ruby/resources/
+  --no-private \
+  --title "AT&T HTML5 SDK (Ruby Server)" \
+  -o ../../../packaged.docs/server/ruby \
+  att/*.rb \
+  att/services/*.rb
 
