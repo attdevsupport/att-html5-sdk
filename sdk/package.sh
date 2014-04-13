c446114e7a1a7bbfba0d26f760b01454248bc3fb
Bruce Williams <brucew@isoftstone.com>
2014-04-13 17:13:32 -0400

    enable java doc generation

--- sdk/package.sh
+++ sdk/package.sh
@@ -8,21 +8,23 @@ echo "building SDK docs"
 rm -Rf ../packaged.docs
 sh buildDocs.sh
 
-echo "building Ruby"
+echo "building Ruby docs"
 cd server/ruby/
 sh buildDocs.sh
 
-echo "building PHP"
+echo "building PHP docs"
 cd ../php/
-#
-# need to find and integrate a tool to generate PHP source
-# documentation before re-enabling this.
-#
-# sh buildDocs.sh
-#
+sh buildDocs.sh
+
+echo "building Java docs"
+cd ../java/
+sh buildDocs.sh
+
 cd ../..
 
-echo "building Java"
+echo "building Java binaries"
 ant -buildfile server/java/build.xml clean package-jar
 
+echo "building SDK"
 ant clean package
+
