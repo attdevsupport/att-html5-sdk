4980f18df9bcb69bd21a2b33d2c704aa10827014
mattcobb <matt@cobbsweb.us>
2014-09-17 11:23:11 -0700

    Show both client token and session tokens - format in HTML

--- sdk/server/java/src/com/html5sdk/att/servlet/AttShowTokensServlet.java
+++ sdk/server/java/src/com/html5sdk/att/servlet/AttShowTokensServlet.java
@@ -1,16 +1,12 @@
 package com.html5sdk.att.servlet;
 
 import java.io.IOException;
-import java.io.Writer;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import com.att.api.oauth.OAuthToken;
 import com.html5sdk.att.AttConstants;
@@ -19,15 +15,15 @@ import com.html5sdk.att.AttConstants;
  * 
  * Return the token for this session
  * 
- * @class com.html5sdk.att.servlet.AttShowSessionTokenServlet
+ * @class com.html5sdk.att.servlet.AttShowTokensServlet
  */
-public class AttShowSessionTokenServlet extends HttpServlet {
+public class AttShowTokensServlet extends ServiceServletBase {
     private static final long serialVersionUID = 1L;
 
     /*
      * @see HttpServlet#HttpServlet()
      */
-    public AttShowSessionTokenServlet() {
+    public AttShowTokensServlet() {
         super();
     }
 
@@ -50,45 +46,52 @@ public class AttShowSessionTokenServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request,
             HttpServletResponse response) throws ServletException, IOException {
 
-        response.setContentType("application/json");
+        response.setContentType("text/html");
 
         // make sure this page never gets cached
         response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
         response.setHeader("Pragma", "no-cache");
         response.setHeader("Expires", "0");
+
+        PrintWriter out = response.getWriter();
         
         try {
         	String scopes[] = new String[3];
         	scopes[0] = "IMMN"; scopes[1] = "MIM"; scopes[2]="DC";
         	int iScope;
             HttpSession session = request.getSession();
-        	JSONObject object = new JSONObject();
         	OAuthToken token = null;
         	String info = null; 
+
+
+
+            out.println("<html><head><title>Tokens</title></head><body>");
+        	
+            try {
+               out.println("clientToken: " + this.credentialsManager.fetchOAuthToken().toBluredString() + "<br>");
+            } catch (Exception fetchEx) {
+               out.println("clientToken: " + "failed<br>");
+            }
         	
         	for(iScope=0; iScope<scopes.length; iScope++) {
         		token = SessionUtils.getTokenForScope(session, scopes[iScope]);
         		if(token!=null) {
         			if(info!=null) {
-        				info += ", " + scopes[iScope] + ": " + token.toBluredString();
+        				info += ",<br> " + scopes[iScope] + ": " + token.toBluredString();
         			} else {
         				info = scopes[iScope] + ": " + token.toBluredString();
         			}
         		}
         	}
-
-            object.put("tokens", info);
             
-            Writer out = response.getWriter();
-            object.write(out);
+            out.println(info);
+            
+            out.println("</body></html>");  
             out.flush();
             out.close();
-        } catch (JSONException se) {
+        } catch (Exception se) {
             try {
-                Writer out = response.getWriter();
-                JSONObject resp = new JSONObject();
-                resp.put(AttConstants.ERROR, se.getMessage());
-                resp.write(out);
+                out.println(AttConstants.ERROR + ": " + se.getMessage());
                 out.flush();
                 out.close();
 
