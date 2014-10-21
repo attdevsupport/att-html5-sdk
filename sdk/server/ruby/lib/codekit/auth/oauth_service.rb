5b955cd09c6728cf704e71f9c09689f313eb46a3
Surender Mohan <smohan36@gmail.com>
2014-10-21 13:41:34 -0700

    copy over revoke token method from internal code kit files

--- sdk/server/ruby/lib/codekit/auth/oauth_service.rb
+++ sdk/server/ruby/lib/codekit/auth/oauth_service.rb
@@ -16,6 +16,8 @@ module Att
 
       #@author kh455g
       class OAuthService 
+        
+        DEFAULT_REVOKE_URL = '/oauth/v4/revoke'
 
         attr_reader :fqdn, :scope, :suburl
         # @!attribute [r] fqdn
@@ -95,6 +97,41 @@ module Att
           end
         end
         protected :getNewToken
+
+        # Revoke a token
+        #
+        # @param token [String] The access_token or refresh_token being revoked
+        # @param token_type [String] What the type the token is
+        #   (:access_token or :refresh_token)
+        # @param opts [Hash] optional parameters 
+        # @param opts [String] :fqdn the url to make the request to
+        # @param opts [Client] :client The client object used to create the
+        #   token
+        # @option opts [String] :revoke_url the suburl to the fqdn for
+        #   requesting tokens, do not change unless you absolutely know what
+        #   you are doing (default: '/oauth/v4/revoke')
+        def revokeToken(token, token_type, opts={})
+          fqdn = (opts[:fqdn] || @fqdn)
+          client = (opts[:client] || @client)
+          token_url = (opts[:revoke_url] || DEFAULT_REVOKE_URL)
+          headers = {
+            :content_type => "application/x-www-form-urlencoded",
+          }
+          params = {
+            :client_id => client.id.to_s,
+            :client_secret => client.secret.to_s,
+            :token => token.to_s,
+            :token_type_hint => token_type.to_s
+          }
+
+          begin
+            response = Transport.post("#{fqdn}#{token_url}", params, headers)
+          rescue RestClient::Exception => e
+            raise(OAuthException, "Problem revoking a token: #{e.response ||
+                                                                e.message}",
+                                                                e.backtrace)
+          end
+        end
 
       end
     end
